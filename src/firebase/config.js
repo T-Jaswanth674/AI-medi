e032c1c8aa4de7e99ea195ee5633639ad8864f8c
THALLURI JASWANTH <2300030674@kluniversity.in>
2026-02-22 20:11:43 +0530

    Enhance comments in Firebase config file
    Added comments for clarity on Firebase configuration.

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,8 @@
 // Firebase configuration
+
+
+// Configure with Your Credentials
+
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "",
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "",
